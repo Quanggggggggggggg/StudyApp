7aeb907593ad741f37fa6f56b621d72f3bbe1f12
Mine <gweng2411@gmail.com>
2025-09-08 20:56:52 +0700

    Login form

---
+++ src/main/java/com/example/appstudy/Controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.example.appstudy.Controller;
+
+import javafx.event.ActionEvent;
+
+public class LoginController {
+    public void handleLogin(ActionEvent actionEvent) {
+    }
+
+    public void goToRegister(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/appstudy/HelloApplication.java
+++ src/main/java/com/example/appstudy/HelloApplication.java
@@ -5,14 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/appstudy/login-register/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        stage.setTitle("IELTS Master");
         stage.setScene(scene);
         stage.show();
     }
